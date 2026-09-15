cc7adb1a30b9429e00d65044ebd25c23dae69c54
Kevin Rivera <kevinriveravergaray@gmail.com>
2026-09-14 22:13:38 -0500

    Add tables part 2

--- DDL/create_database.sql
+++ DDL/create_database.sql
@@ -12,4 +12,50 @@ nombre VARCHAR (55) NOT NULL,
 descripcion VARCHAR (255) NULL
 );
 
----- Tabla canalesCREATE TABLE canales(	id INT IDENTITY(1,1) PRIMARY KEY,	codigo CHAR(6) UNIQUE NOT NULL,	nombre VARCHAR(55) NOT NULL,	descripcion VARCHAR (255) NULL);--- tabla categoria productos create table categoria_productos(	id int identity(1,1) primary key,	codigo char(6) unique not null,	nombre varchar(55) not null,	descripcion varchar(255) null);
+---- Tabla canales
+
+CREATE TABLE canales(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	codigo CHAR(6) UNIQUE NOT NULL,
+	nombre VARCHAR(55) NOT NULL,
+	descripcion VARCHAR (255) NULL
+);
+
+
+--- tabla categoria productos 
+
+create table categoria_productos(
+	id int identity(1,1) primary key,
+	codigo char(6) unique not null,
+	nombre varchar(55) not null,
+	descripcion varchar(255) null
+);
+
+
+--- Productos
+
+CREATE TABLE productos(
+	id INT IDENTITY (1,1) PRIMARY KEY,
+	codigo CHAR(6) UNIQUE NOT NULL,
+	nombre VARCHAR(55) NOT NULL,
+	categoria_id INT NOT NULL,
+	tipo_producto VARCHAR(30) NULL,
+	moneda VARCHAR(25) NULL,
+	estado VARCHAR(25) NULL,
+	CONSTRAINT fk_categoria_productos FOREIGN KEY (categoria_id) REFERENCES categoria_productos(id)
+	);
+
+--- clientes
+
+CREATE TABLE clientes (
+	id INT IDENTITY (1,1) PRIMARY KEY,
+	codigo CHAR(6) UNIQUE NOT NULL,
+	tipo_cliente VARCHAR(1) NOT NULL,
+	fecha_alta DATE DEFAULT(CONVERT(DATE,GETDATE())),
+	estado VARCHAR (25) NOT NULL,
+	segmento_id INT NOT NULL,
+	FOREIGN KEY (segmento_id) REFERENCES segmentos(id)
+);
+
+EXEC SP_HELP clientes;
+
